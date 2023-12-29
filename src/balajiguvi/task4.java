ea61ad02e02e5968d7f3d53105327ad078d5e326
BALAJIJFWD <aibalaji657@gmail.com>
2023-12-29 20:49:20 +0530

    task 4

---
+++ src/balajiguvi/task4.java
@@ -0,0 +1,18 @@
+package balajiguvi;
+
+public class task4 {
+
+	public static void main(String[] args) {
+
+		double number = 16.7;
+        if (number < 0.0)
+            System.out.println(number + " is a negative number");
+
+        else if ( number > 0.0)
+            System.out.println(number + " is a positive number");
+
+        else
+            System.out.println(number + " is 0.");
+	}
+
+}
